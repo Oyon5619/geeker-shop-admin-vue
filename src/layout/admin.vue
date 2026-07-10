7ecb9a6aaa75a75f8303567e7d1dbb166e2d3d17
YONSEN <oyx20020328@gmail.com>
2026-07-10 23:40:04 +0800

    feat 增加Transition页面切换过渡动画

--- src/layout/admin.vue
+++ src/layout/admin.vue
@@ -11,16 +11,60 @@ import TabList from "./components/tabList.vue";
     </n-layout-header>
     <n-layout class="pt-14 h-full" has-sider>
       <AppMenu />
-      <n-layout-content class="h-full p-4">
-        <TabList />
-        <RouterView v-slot="{ Component }">
-          <KeepAlive max="10">
-            <component :is="Component"></component>
-          </KeepAlive>
-        </RouterView>
+      <n-layout-content>
+        <n-flex class="border h-full p-4 overflow-hidden" vertical>
+          <TabList />
+          <RouterView v-slot="{ Component }">
+            <div class="main-container">
+              <Transition name="fade">
+                <KeepAlive max="10">
+                  <component :is="Component"></component>
+                </KeepAlive>
+              </Transition>
+            </div>
+          </RouterView>
+        </n-flex>
       </n-layout-content>
     </n-layout>
   </n-layout>
 </template>
 
-<style scoped></style>
+<style scoped>
+@reference "@/styles/index.css";
+
+.main-container {
+  @apply flex-1 overflow-x-hidden overflow-y-auto scrollbar-thin scrollbar-thumb-white;
+}
+
+/** 下面这些类选择器配合Transition组件用 */
+.fade-enter-from {
+  opacity: 0;
+  -webkit-transform: translate3d(100%, 0, 0);
+  transform: translate3d(100%, 0, 0);
+}
+
+.fade-enter-to {
+  opacity: 1;
+  -webkit-transform: translate3d(0, 0, 0);
+  transform: translate3d(0, 0, 0);
+}
+
+.fade-leave-from {
+  opacity: 1;
+}
+
+.fade-leave-to {
+  opacity: 0;
+  -webkit-transform: translate3d(-100%, 0, 0);
+  transform: translate3d(-100%, 0, 0);
+}
+
+.fade-enter-active,
+.fade-leave-active {
+  transition: all 0.35s;
+}
+
+.fade-enter-active {
+  transition-delay: 0.35s;
+}
+</style>
--- src/pages/goods/goodList.vue
+++ src/pages/goods/goodList.vue
@@ -1,3 +1,3 @@
 <template>
-  <div>商品列表</div>
+  <div class="bg-cyan-200 min-h-full">商品列表</div>
 </template>
