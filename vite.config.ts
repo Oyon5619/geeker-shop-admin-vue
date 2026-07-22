b81ae6bad00a8ca34bf02647f6b8d81fd0ad4dce
YONSEN <oyx20020328@gmail.com>
2026-07-22 13:25:03 +0800

    解决打包后页面控制台报错c.then is not a function

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import vue from "@vitejs/plugin-vue";
+import { visualizer } from "rollup-plugin-visualizer";
 
 import path from "path";
 
@@ -11,41 +12,89 @@ import { NaiveUiResolver } from "unplugin-vue-components/resolvers";
 // 配置TailwindCss
 import TailwindCssVite from "@tailwindcss/vite";
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [
-    vue(),
-    TailwindCssVite(),
-    AutoImport({
-      imports: [
-        "vue",
-        {
-          "naive-ui": [
-            "useDialog",
-            "useMessage",
-            "useNotification",
-            "useLoadingBar",
-          ],
-        },
-      ],
-    }),
-    Components({
-      resolvers: [NaiveUiResolver()],
-    }),
-  ],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src/"),
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd());
+  const { VITE_BASE_URL, VITE_PROXY_API } = env;
+
+  return {
+    base: VITE_BASE_URL,
+    plugins: [
+      vue(),
+      TailwindCssVite(),
+      AutoImport({
+        imports: [
+          "vue",
+          {
+            "naive-ui": [
+              "useDialog",
+              "useMessage",
+              "useNotification",
+              "useLoadingBar",
+            ],
+          },
+        ],
+      }),
+      Components({
+        resolvers: [NaiveUiResolver()],
+      }),
+      visualizer({
+        open: true, // 打包后自动代开分析图
+        filename: "./distAnalyse/visualizer.html",
+        gzipSize: true, // 显示gzip压缩大小
+        brotliSize: true, // 显示brotliSize压缩大小
+      }),
+    ],
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src/"),
+      },
+      extensions: [".js", ".ts", ".vue"],
     },
-    extensions: [".js", ".ts", ".vue"],
-  },
-  server: {
-    proxy: {
-      "/api": {
-        target: "http://ceshi13.dishait.cn",
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ""),
+    server: {
+      proxy: {
+        "/api": {
+          target: VITE_PROXY_API,
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/api/, ""),
+        },
       },
     },
-  },
+    // Optimization
+    build: {
+      // sourcemap: true,
+      rolldownOptions: {
+        output: {
+          // 静态文件按类型分包
+          chunkFileNames: "static/js/[name]-[hash].js",
+          entryFileNames: "static/js/[name]-[hash].js",
+          assetFileNames: "static/[ext]/[name]-[hash].[ext]",
+          codeSplitting: {
+            groups: [
+              {
+                name: "vue-vendor",
+                test: /node_modules[\\/]vue/,
+                priority: 20,
+              },
+              {
+                name: "ui-vendor",
+                test: /node_modules[\\/]naive-ui/,
+                priority: 15,
+              },
+              {
+                name: "vender",
+                test: /node_modules/,
+                priority: 10,
+              },
+              {
+                name: "common",
+                minShareCount: 2,
+                minSize: 10000,
+                priority: 5,
+              },
+            ],
+          },
+        },
+      },
+    },
+  };
 });
